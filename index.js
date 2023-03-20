1f0e2c024620f22752a1fabc8ce93aa289f13c12
Megan Dacus <meganjustice0609@gmail.com>
2023-03-20 12:50:49 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let main = document.querySelector('main');
+main.remove();
+
+let newHeader = document.createElement('h1')
+document.body.append(newHeader)
+newHeader.id = 'victory'
+newHeader.textContent = 'Megan is the champion'
+
+console.log(`This is the ${newHeader.nodeName}`)
